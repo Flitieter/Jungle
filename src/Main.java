cf2f109f436c280aa42add1aaf2612505e4b273e
FeTieTer <3327965195@qq.com>
2023-05-13 00:35:36 +0800

    变更

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,7 @@ import java.io.IOException;
 public class Main {
 
     public static void main(String[] args) {
+        int xxxx;
         SwingUtilities.invokeLater(() -> {
             StartFrame startFrame=new StartFrame();
             startFrame.setVisible(true);
